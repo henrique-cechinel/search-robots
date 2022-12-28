d879c6e4cdb29121312b6063d06605f458da1d73
Henrique Cechinel <henrique-cechinel@hotmail.com>
2022-12-27 21:10:15 -0300

    Updated from fixed robots list to dynamic list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { robots } from './robots';
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import './App.css'
@@ -8,14 +7,19 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users }));
+  }
+
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
-    console.log(this.state.searchfield)
   }
   
   render() {
